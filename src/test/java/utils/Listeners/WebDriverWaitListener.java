aeb992547861a3d4c0ef1c2e0b4d9726cbe1c4d6
MartinT <martin.tellechea@gmail.com>
2022-04-11 12:28:15 -0300

    Added highlighted elements for UI tests

--- src/test/java/utils/Listeners/WebDriverWaitListener.java
+++ src/test/java/utils/Listeners/WebDriverWaitListener.java
@@ -2,6 +2,7 @@ package utils.Listeners;
 
 import BaseTest.BaseTest;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
@@ -13,25 +14,28 @@ public class WebDriverWaitListener extends AbstractWebDriverEventListener {
     private static final int TIMEOUT_IN_SECONDS=30;
     private final WebDriverWait wait;
     public Logger log = Logger.getLogger(BaseTest.class.getName());
+    private JavascriptExecutor js;
 
 
     public WebDriverWaitListener(WebDriver driver) {
         wait = new WebDriverWait(driver,TIMEOUT_IN_SECONDS);
+        js = (JavascriptExecutor) driver;
     }
 
     public void beforeClickOn(WebElement element, WebDriver driver) {
+        js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
         long startTime = System.currentTimeMillis();
-        log.info("beforeClickOn:: start");
+        log.info("beforeClickOn '"+element.getText()+"':: start");
         wait.until(ExpectedConditions.elementToBeClickable(element));
         long endTime = System.currentTimeMillis();
-        log.info("beforeClickOn:: end, took "+(endTime-startTime)+" milliseconds");
+        log.info("beforeClickOn '"+element.getText()+"':: end, took "+(endTime-startTime)+" milliseconds");
     }
 
     public void beforeGetText(WebElement element, WebDriver driver) {
         long startTime = System.currentTimeMillis();
-        log.info("beforeGetText:: start");
+        log.info("beforeGetText '"+element.getText()+"':: start");
         wait.until(ExpectedConditions.visibilityOf(element));
         long endTime = System.currentTimeMillis();
-        log.info("beforeGetText:: end, took "+(endTime-startTime)+" milliseconds");
+        log.info("beforeGetText '"+element.getText()+"':: end, took "+(endTime-startTime)+" milliseconds");
     }
 }
