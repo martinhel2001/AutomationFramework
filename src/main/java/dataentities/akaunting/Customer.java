70f0a4fd9f3a1c435701fc382ddc05041c814fa7
Kyrevam <guilllermo.tellechea@hotmail.com>
2023-12-28 17:11:45 -0300

    Changed webelements in page 01

--- src/main/java/dataentities/akaunting/Customer.java
+++ src/main/java/dataentities/akaunting/Customer.java
@@ -7,7 +7,7 @@ public class Customer {
     @Setter @Getter private String name ="";
     @Setter @Getter private String Address="123 Elm Street";
     @Setter @Getter private String city="Toronto";
-    @Setter @Getter private String state="TO";
+    @Setter @Getter private String state="Misiones";
     @Setter @Getter private String country="Angola";
     @Setter @Getter private String currency = "Euro";
     @Setter @Getter private String Email;
