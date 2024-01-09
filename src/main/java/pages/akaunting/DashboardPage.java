9909e33686230bd1b16bd201a487dea45e9ed090
Kyrevam <guilllermo.tellechea@hotmail.com>
2024-01-09 12:12:54 -0300

    Added Menu locators in DashboardPage

--- src/main/java/pages/akaunting/DashboardPage.java
+++ src/main/java/pages/akaunting/DashboardPage.java
@@ -6,16 +6,48 @@ import pages.BasePageInteractions;
 
 public class DashboardPage extends BasePageInteractions {
 
-    private By SalesMnu = By.linkText("Sales");
-    private By CustomersSubMnu = By.linkText("Customers");
+    private By SalesMenu = By.linkText("Sales");
+    private By CustomersSubMenu = By.linkText("Customers");
+
+    private By DashboardMenu = By.linkText("Dashboard");
+
+    private By ItemsMenu = By.linkText("Items");
+
+    private By InvoicesSubMenu = By.linkText("Invoices");
+
+    private By RevenuesSubMenu = By.linkText("Revenues");
+
+    private By PurchasesMenu = By.linkText("Purchases");
+
+    private By BillsSubMenu = By.linkText("Bills");
+
+    private By PaymentsSubMenu = By.linkText("Payments");
+
+    private By VendorsSubMenu = By.linkText("Vendors");
+
+    private By BankingMenu = By.linkText("Banking");
+
+    private By AccountsSubMenu = By.linkText("Accounts");
+
+    private By TransfersSubMenu = By.linkText("Transfers");
+
+    private By TransactionsSubMenu = By.linkText("Transactions");
+
+    private By ReconciliationsSubMenu = By.linkText("Reconciliations");
+
+    private By ReportsMenu = By.linkText("Reports");
+
+    private By SettingsMenu = By.linkText("Settings");
+
+    private By AppsMenu = By.linkText("Apps");
 
     public DashboardPage(EventFiringWebDriver session) {
         super(session);
     }
 
     public void goToCustomers() {
-        eventDriver.findElement(SalesMnu).click();
-        eventDriver.findElement(CustomersSubMnu).click();
+        eventDriver.findElement(SalesMenu).click();
+        eventDriver.findElement(CustomersSubMenu).click();
     }
 }
 
