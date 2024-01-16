7f54c56e703b900dffd2c4d63fec559d1f5c7bde
Kyrevam <guilllermo.tellechea@hotmail.com>
2024-01-16 13:20:06 -0300

    Added methods to newInvoicePage

--- src/main/java/dataentities/akaunting/Customer.java
+++ src/main/java/dataentities/akaunting/Customer.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Customer {
-    @Setter @Getter private String name ="";
-    @Setter @Getter private String Address="123 Elm Street";
+    @Getter private String name ="";
+    @Getter private String Address="123 Elm Street";
     @Setter @Getter private String city="Toronto";
     @Setter @Getter private String state="Misiones";
     @Setter @Getter private String country="Angola";
